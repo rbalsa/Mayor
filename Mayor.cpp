adee7d9509e02987d520f69088e6992149a5884f
Ramiro Balsa <ramiro.balsa@live.com>
2017-04-26 09:49:23 -0300

    Add files via upload

---
+++ Mayor.cpp
@@ -0,0 +1,62 @@
+/*Dado dos números informar el mayor
+ *Ramiro Balsa
+ *Melina Cuellar
+ *Agustin Sebastian
+ *Yamil Alejandro Lopez
+ *Ignacio Muñoz	
+ *09042017
+ */
+ 
+ 
+ 
+ #include <iostream>
+ #include <string>
+ #include <string>
+ 
+ using namespace std;
+ 
+ 
+ int main (){
+ 	
+	 string i,j; 	
+ 	
+ 	std :: cout << "Ingrese un numero "  ;
+ 	
+ 	std :: cin >> i;
+ 	
+ 	std :: cout << "\nIngrese otro numero " ;
+ 	
+ 	std :: cin >> j;
+	 	 
+
+ 	if (i>j){	 
+	 
+ 		
+ 		std :: cout << "\nEl numero mayor es\n" <<i<< endl;
+ 		
+ }
+    
+ 
+ 
+ 	else if (i<j){
+ 		
+ 		
+ 		std :: cout <<"\nEl numero mayor es " <<j<< endl;
+}
+ 		
+
+	 
+ 	else if(i==j){
+	 
+	 	  		
+ 		std :: cout << "\nNo existe numero mayor, los numeros son iguales\n" << endl;
+ 		
+ 		}
+ 		
+ 		
+ 	 
+ 		
+  	
+  	 	
+ return 0;
+}  
